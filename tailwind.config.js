de2312719d3643bc7f66888b955d1c2f20ba7a72
Pedro Miguel Pereira <pedromcdp@gmail.com>
2022-06-02 16:25:31 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        verde: "#37B780",
+      },
+      fontFamily: {
+        body: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
